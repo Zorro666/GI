1eb62d9752bb6c062d01228119304683f944dc2a
Jake <jake@evansturner.co.uk>
2012-05-30 05:54:24 +0100

    wip player parse - need to test it and add error checking to parse functions need to error if not all values get loaded need to error if find an unexpected value

--- gi_Player.c
+++ gi_Player.c
@@ -16,6 +16,8 @@ void gi_Player_Init(gi_Player* const pThis)
 
 GI_Return gi_Player_Load(gi_Player* const pThis, const Json_Value* const root)
 {
+	Json_Value* it;
+
 	if (root->m_type != JSON_OBJECT)
 	{
 		fprintf(stderr, "NOT JSON_OBJECT\n");
@@ -33,7 +35,48 @@ GI_Return gi_Player_Load(gi_Player* const pThis, const Json_Value* const root)
 
 	gi_Player_Init(pThis);
 
-	return GI_ERROR;
+	for (it = root->m_first_child; it != NULL; it = it->m_next_sibling)
+	{
+		if (it->m_type == JSON_STRING)
+		{
+			if (strcmp(it->m_name, "Name") == 0)
+			{
+				strncpy(pThis->m_name, it->m_value_data.string_value, MAX_PLAYERNAME_SIZE);
+			}
+			if (strcmp(it->m_name, "Position") == 0)
+			{
+				strncpy(pThis->m_position, it->m_value_data.string_value, MAX_POSITION_SIZE);
+			}
+		}
+		if (it->m_type == JSON_INT)
+		{
+			if (strcmp(it->m_name, "Level") == 0)
+			{
+				pThis->m_level = it->m_value_data.int_value;
+			}
+			if (strcmp(it->m_name, "Q") == 0)
+			{
+				pThis->m_qst[GI_Q] = it->m_value_data.int_value;
+			}
+			if (strcmp(it->m_name, "S") == 0)
+			{
+				pThis->m_qst[GI_S] = it->m_value_data.int_value;
+			}
+			if (strcmp(it->m_name, "T") == 0)
+			{
+				pThis->m_qst[GI_T] = it->m_value_data.int_value;
+			}
+		}
+		if (it->m_type == JSON_FLOAT)
+		{
+			if (strcmp(it->m_name, "Experience") == 0)
+			{
+				pThis->m_experience = it->m_value_data.float_value;
+			}
+		}
+	}
+
+	return GI_SUCCESS;
 }
 
 void gi_Player_Print(gi_Player* const pThis)
