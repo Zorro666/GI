e97bbc9941a657b0663ea541b823919cd68a3b19
Jake <jake@evansturner.co.uk>
2012-06-11 06:20:26 +0100

    compute defence

--- gi_PlayInfo.h
+++ gi_PlayInfo.h
@@ -27,6 +27,7 @@ GI_Return gi_PlayInfo_AddOffencePlay(gi_PlayInfo* const pThis, gi_OffencePlay* c
 void gi_PlayInfo_ComputeOffenceBase(gi_PlayInfo* const pThis, const gi_Player* const pPlayer, const size_t playerIndex);
 
 GI_Return gi_PlayInfo_AddDefencePlay(gi_PlayInfo* const pThis, gi_DefencePlay* const pDefencePlay);
+void gi_PlayInfo_ComputeDefenceBase(gi_PlayInfo* const pThis, const gi_Player* const pPlayer, const size_t playerIndex);
 
 void gi_PlayInfo_ComputeSpecialTeams(gi_PlayInfo* const pThis, const gi_Player* const pPlayer, const size_t playerIndex);
 
